39a92e62cbd806b5074544c23c895716c6cceb79
ujjwal <ujjwals346@gmail.com>
2025-07-14 17:58:18 +0530

    issues fixed

--- config/database.ts
+++ config/database.ts
@@ -73,24 +73,34 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: false,
+        ssl: env.bool('DATABASE_SSL', false),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
 
     postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: false,
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
+      connection: env('DATABASE_URL')
+        ? {
+            connectionString: env('DATABASE_URL'),
+            ssl: env.bool('DATABASE_SSL', false)
+              ? { rejectUnauthorized: false }
+              : false,
+            schema: env('DATABASE_SCHEMA', 'public'),
+          }
+        : {
+            host: env('DATABASE_HOST', 'localhost'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'strapi'),
+            user: env('DATABASE_USERNAME', 'strapi'),
+            password: env('DATABASE_PASSWORD', 'strapi'),
+            ssl: env.bool('DATABASE_SSL', false)
+              ? { rejectUnauthorized: false }
+              : false,
+            schema: env('DATABASE_SCHEMA', 'public'),
+          },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
+    
 
     sqlite: {
       connection: {
@@ -108,3 +118,4 @@ export default ({ env }) => {
     },
   };
 };
+
